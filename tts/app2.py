1fdeb5464edee3f3900413851852c3ed5bd73122
Ali Badshah <ali.accessideas@gmail.com>
2025-06-20 13:44:02 +0530

    link to backend

--- tts/app2.py
+++ tts/app2.py
@@ -467,4 +467,4 @@ async def transcribe(
 if __name__ == "__main__":  # pragma: no cover
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 10000)), reload=True)
